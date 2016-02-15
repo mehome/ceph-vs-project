5626f8ed2427797e16be331eb98f422c82419134
shines77 <gz_shines@msn.com>
2016-02-15 18:09:57 +0800

    又添加了不少ceph的文件, 比较完整了.
    Signed-off-by: shines77 <gz_shines@msn.com>

--- vc2015/src/cephd/cephd.cpp
+++ vc2015/src/cephd/cephd.cpp
@@ -9,10 +9,14 @@
 
 int main(int argc, char * argv[])
 {
+    long double f = 1.0;
+    printf("f = %lf\n", f);
+    printf("sizeof(long double) = %u\n", sizeof(f));
     std::atomic<int> counter;
     counter.fetch_add(1, std::memory_order_seq_cst);
     counter.fetch_add(1, std::memory_order_seq_cst);
 
     MemoryBarrier();
+    ::system("pause");
     return 0;
 }
